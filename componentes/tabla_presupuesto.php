55b8233bcdb6e4ec310d3a594d000952cf717289
Gabriel de la Calle <gdelacalle@gmail.com>
2023-10-04 12:54:25 -0300

    otra

--- componentes/tabla_presupuesto.php
+++ componentes/tabla_presupuesto.php
@@ -28,23 +28,26 @@
                 $pesounidad=0;
                 $total=0;
                 
+                
                     require_once("../includes/db.php");
                     $result = mysqli_query($conexion, "SELECT cast(SUM(p.cantidad * p.metros * e.peso)as decimal(10,2)) as total, 
-                    p.id, cast((e.peso*p.metros)as decimal(10,2))as peso,e.seccion,i.producto,p.cantidad,p.metros,p.unidad,o.descripcion,
-                    o.id_cliente
-                    FROM presupuestotemporal as p INNER JOIN inventario as i INNER JOIN equivalencias as e INNER JOIN obras as o 
-                    ON p.id_producto = i.id  AND p.id_obra = o.id AND p.id_obra = $id GROUP BY p.id ORDER BY p.id ASC");   
+                    p.id, cast((e.peso*p.metros)as decimal(10,2))as peso,e.seccion,i.producto,p.cantidad,p.metros,p.unidad,
+                    o.descripcion,p.id_seccion as sid,o.id_cliente 
+                    FROM presupuestotemporal as p INNER JOIN inventario as i INNER JOIN equivalencias as e 
+                    INNER JOIN obras as o ON p.id_producto = i.id AND p.seccion = e.seccion AND p.id_obra = $id 
+                    GROUP BY p.id ORDER BY p.id ASC");   
                 while ($fila = mysqli_fetch_assoc($result)) :                       
                 ?>
                 <tr>
                     <td width="20%"><?php echo $fila['descripcion']; ?></td>
                     <td width="20%"><?php echo $fila['producto']; ?></td>
+                    <td width="20%"><?php echo $fila['sid']; ?></td>
                     <td width="20%"><?php echo $fila['seccion']; ?></td>
                     <td width="5%"><?php echo $fila['cantidad']; ?></td>
                     <td width="5%"><?php echo $fila['metros']; ?></td>
                     <td width="5%"><?php echo $fila['unidad']; ?></td>
-                    <td width="10%"><?php echo $fila['pesoxunidad']; ?></td>
-                    <td width="10%"><?php echo $fila['totalpeso']; ?></td>
+                    <td width="10%"><?php echo $fila['peso']; ?></td>
+                    <td width="10%"><?php echo $fila['total']; ?></td>
                     <td><a href="../includes/eliminar_item_presupuesto.php?id=<?php echo $fila['id']?>" class="btn btn-danger btn-del"><i class="fa fa-trash "></i></a></td>
                 </tr>
                 <?php $granTotal += $fila['totalpeso'];?>
