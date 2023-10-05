911c6e20ad26525ea3c1a1d2d4ec4d026f2b7300
hernanbravi <61250970+hernanbravi@users.noreply.github.com>
2023-10-05 20:25:33 -0300

    consulta

--- componentes/tabla_presupuesto.php
+++ componentes/tabla_presupuesto.php
@@ -31,9 +31,8 @@
                 
                     require_once("../includes/db.php");
                     $result = mysqli_query($conexion, "SELECT cast(SUM(p.cantidad * p.metros * e.peso)as decimal(10,2)) as total, 
-                    p.id, cast((e.peso*p.metros)as decimal(10,2))as peso,e.seccion,i.producto,p.cantidad,p.metros,p.unidad,
-                    o.descripcion,p.id_seccion as sid,o.id_cliente 
-                    FROM presupuestotemporal as p INNER JOIN inventario as i INNER JOIN equivalencias as e 
+                    p.id, cast((e.peso*p.metros) as decimal(10,2)) as peso,e.id as sid,e.seccion,i.producto,p.cantidad,p.metros,p.unidad,
+                    o.descripcion,p.id_seccion as sid,o.id_cliente FROM presupuestotemporal as p INNER JOIN inventario as i INNER JOIN equivalencias as e 
                     INNER JOIN obras as o ON p.id_producto = i.id AND p.seccion = e.seccion AND p.id_obra = $id 
                     GROUP BY p.id ORDER BY p.id ASC");   
                 while ($fila = mysqli_fetch_assoc($result)) :                       
